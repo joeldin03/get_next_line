70798fded3ed3f690e6a02879ab50d5e48b891af
joeldin03 <joel_lozano10@hotmail.com>
2024-02-13 20:51:40 +0100

    project added

---
+++ get_next_line.c
@@ -0,0 +1,76 @@
+#include "get_next_line.h"
+
+static char *set_line(char *buff)
+{
+    char    *stash;
+    size_t  i;
+
+    i = 0;
+    while (buff[i] != '\n' && buff[i] != '\0')
+        i++;
+    if (buff[i] == 0)
+        return (NULL);
+    stash = ft_substr(buff, i + 1, ft_strlen(buff) - i);
+    if (!(*stash))
+    {
+        free(stash);
+        stash = NULL;
+    }
+    buff[i + 1] = 0;
+    return (stash);
+}
+
+static char *add_new_line(int fd, char *stash, char *buff)
+{
+    int     b_read;
+    char    *tmp;
+
+    b_read = 1;
+    while (b_read)
+    {
+        b_read = read(fd, buff, BUFFER_SIZE);
+        if (b_read == -1)
+        {
+            free(stash);
+            return (NULL);
+        }
+        else if (b_read == 0)
+            break ;
+        buff[b_read] = 0;
+        if (!stash)
+            stash = ft_strdup("");
+        tmp = stash;
+        stash = ft_strjoin(tmp, buff);
+        free(tmp);
+        tmp = NULL;
+        if (ft_strchr(buff, '\n'))
+            break ;
+    }
+    return (stash);
+}
+
+char    *get_next_line(int fd)
+{
+    static char *stash;
+    char        *line;
+    char        *buff;
+
+    buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+    if (!buff)
+        return (NULL);
+    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+    {
+        free(stash);
+        free(buff);
+        stash = NULL;
+        buff = NULL;
+        return (NULL);
+    }
+    line = add_new_line(fd, stash, buff);
+    free(buff);
+    buff = NULL;
+    if (!line)
+        return (NULL);
+    stash = set_line(stash);
+    return (line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,18 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 20
+# endif
+# include <stdlib.h>
+# include <sys/types.h>
+# include <unistd.h>
+
+char    *get_next_line(int fd);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *str, char c);
+
+#endif
