c02c1d62973046f6074029eff7ff031055d71191
joeldin03 <joel_lozano10@hotmail.com>
2024-02-13 21:15:39 +0100

    para hacer pruebas

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include "get_next_line.h"
+
+int main()
+{
+    int fd;
+    char *line;
+
+    fd = open("example.txt", O_RDONLY);
+    line = get_next_line(fd);
+    while (line)
+    {
+        printf("Line read: %s\n", line);
+        free(line);
+    }
+    close(fd);
+    return 0;
+}
