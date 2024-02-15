e3a64b85dd061c7898d3c216500c492035c24f5f
joeldin03 <148290328+joeldin03@users.noreply.github.com>
2024-02-15 17:47:52 +0100

    para hacer pruebas

---
+++ main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joelozan <joelozan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 17:34:19 by joelozan          #+#    #+#             */
+/*   Updated: 2024/02/15 17:46:51 by joelozan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line.h"
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("text.txt", O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		printf("%s", line);
+		free(line);
+	}
+	printf("\n%s", line);
+	close(fd);
+	return (0);
+}
